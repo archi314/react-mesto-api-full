0bdfbd6eec51336d5238c6c53bb41624ce721ac5
Artem Stukalov <artem.s95@mail.ru>
2022-10-03 04:39:04 +0300

    Проектная работа 15.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { CookiesProvider } from "react-cookie";
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <CookiesProvider>
       <BrowserRouter>
           <App />
       </BrowserRouter>
+    </CookiesProvider>
 );
 
 
